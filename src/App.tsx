9e3888178c9c88f6ea601f988802243566518b3c
omeneghel <trafego.meneghel@gmail.com>
2025-11-09 04:04:35 -0300

    Update App.tsx
    test 1

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/:slug" element={<IndexDynamic />} />
+          <Route path="/:slug" element={<div style={{ padding: 24, fontSize: 20 }}>Router OK</div>} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
